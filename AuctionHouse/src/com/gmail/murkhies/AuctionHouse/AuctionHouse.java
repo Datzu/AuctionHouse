9a6f50b61ff3508ac9ee7c6797eac4ddedf96a2e
Degor <Degor@192.168.0.195>
2014-01-11 18:58:50 +0100

    Added plugin.yml

--- AuctionHouse/src/com/gmail/murkhies/AuctionHouse/AuctionHouse.java
+++ AuctionHouse/src/com/gmail/murkhies/AuctionHouse/AuctionHouse.java
@@ -1,5 +1,27 @@
 package com.gmail.murkhies.AuctionHouse;
 
-public class AuctionHouse {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AuctionHouse extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		getLogger().info("AuctionHouse is loading...");
+	}
+
+	@Override
+	public void onDisable() {
+		getLogger().info("AuctionHouse is disabling...");
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("ah")) {
+			
+			return true;
+		}
+		return false;
+	}
 
 }
